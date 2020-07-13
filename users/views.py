d3ecf172364f351ab644ab2c983f5abb926b8878
Nikhil Kataria <44164498+katarianikhilk@users.noreply.github.com>
2020-07-13 17:22:59 +0530

    creating test conflict

--- users/views.py
+++ users/views.py
@@ -43,4 +43,4 @@ def profile(request):
         'p_form': p_form
     }
 
-    return render(request, 'users/profile.html', context)
+    return render(request, 'users/profile.html', context)dasffsafasdfadfafasddfdsafsdfasdfsadfsadafdsaafsadfasddfasdfsadfasdfasd
